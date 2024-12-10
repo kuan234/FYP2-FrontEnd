33e1a9114ee22bfc9500b6aaa711b8319f6b51c1
kuan234 <1201202314@student.mmu.edu.my>
2024-12-11 01:00:50 +0800

    updated

--- app/(screens)/_layout.js
+++ app/(screens)/_layout.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Layout() {
   useEffect(() => {
       setTimeout(() => {
-      setStatusBarStyle("dark");
+      setStatusBarStyle("light");
     }, 0);
   }, []);
   return (
