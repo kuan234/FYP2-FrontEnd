811b703419d54c065a494fd6bc5cf37a5521dc32
kuan234 <1201202314@student.mmu.edu.my>
2024-12-30 19:06:56 +0800

    add edit profile icon, get admin role

--- app/(screens)/dashboard.js
+++ app/(screens)/dashboard.js
@@ -1,90 +1,154 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; // Import icons
+import { Menu, MenuOptions, MenuOption, MenuTrigger, MenuProvider } from 'react-native-popup-menu';
+import axios from 'axios';
 
 export default function DashboardScreen() {
   // Extract parameters passed via route
   const params = useLocalSearchParams();
   const { name, id } = params; // Get `name` and `id` from route params
   console.log('Dashboard Params:', params); // Debug: Log received params
-
+  
+  const [role, setRole] = useState('');
+  const [isCheckedIn, setIsCheckedIn] = useState(false); // State to track check-in status
+  const [isCheckedOut, setIsCheckedOut] = useState(false); // State to track check-out status
   const router = useRouter(); // Initialize the router
+  const serverIP = '192.168.0.105'; // Replace with your server IP
+
   const navigateToCamera = () => {
+    if (!isCheckedIn) {
+      setIsCheckedIn(true); // Set check-in status
+    } else if (isCheckedIn && !isCheckedOut) {
+      setIsCheckedOut(true); // Set check-out status
+    }
     router.push({
       pathname: '/(screens)/camera',
       params: { id, name },
     });//navigate to camera screen
-
   };
+
   const navigateToUserList = () => {
     router.push({
       pathname: '/(screens)/userlist',
       params: { id, name },
     });//navigate to user screen
   };
-  const navigateToattendance = () => {
+
+  const navigateToAttendance = () => {
     router.push({
       pathname: '/(screens)/attendance',
       params: { id, name },
     });//navigate to user screen
   };
 
-  return (
-    <View style={styles.container}>
-      {/* Header */}
-      <View style={styles.header}>
-        <Text style={styles.username}>{name ? name : 'Guest'}</Text>
-        <Image
-          source={{ uri: 'https://via.placeholder.com/50' }} // Replace with profile image URL
-          style={styles.profileImage}
-        />
-      </View>
+  const navigateToEditProfile = () => {
+    router.push({
+      pathname: '/(screens)/editprofile',
+      params: { id, name },
+    });
+  };
 
-      {/* Buttons */}
-      <View style={styles.buttonContainer}>
-          {/* Check In Button */}
+  const navigateToUpdateTimes = () => {
+    router.push({
+      pathname: '/(screens)/updateTimes',
+      params: { id, name },
+    });
+  };
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const response = await axios.get(`http://${serverIP}:8000/get_user_role/${id}/`);
+        if (response.data.role) {
+          setRole(response.data.role);
+        }
+      } catch (error) {
+        console.error('Error fetching user role:', error);
+      }
+    };
+
+    const fetchCheckInStatus = async () => {
+      try {
+        const response = await axios.get(`http://${serverIP}:8000/get_check_in_status/${id}/`);
+        if (response.data.checked_in !== undefined) {
+          setIsCheckedIn(response.data.checked_in);
+          setIsCheckedOut(response.data.checked_out);
+        }
+      } catch (error) {
+        console.error('Error fetching check-in status:', error);
+      }
+    };
+
+    fetchUserRole();
+    fetchCheckInStatus();
+  }, [id]);
+
+  return (
+    <MenuProvider>
+      <View style={styles.container}>
+        {/* Header */}
+        <View style={styles.header}>
+          <Text style={styles.username}>{name ? name : 'Guest'}</Text>
+          <Menu>
+            <MenuTrigger>
+              <MaterialCommunityIcons name="menu" size={30} color="black" style={styles.menuIcon} />
+            </MenuTrigger>
+            <MenuOptions customStyles={optionsStyles}>
+              <MenuOption onSelect={navigateToEditProfile} text='Edit Profile' />
+              {/* Add more menu options here if needed */}
+            </MenuOptions>
+          </Menu>
+        </View>
+
+        {/* Buttons */}
+        <View style={styles.buttonContainer}>
+          {/* Check In/Out Button */}
           <TouchableOpacity 
-            style={[styles.button, styles.checkInButton]} 
+            style={[styles.button, isCheckedIn && !isCheckedOut ? styles.checkOutButton : styles.checkInButton]} 
             onPress={navigateToCamera} // Navigate to camera screen on press
           >
-            <MaterialCommunityIcons name="clock" size={24} color="white" />
-            <Text style={styles.buttonText}>Check In</Text>
-            <Text style={styles.buttonDescription}>Check in for Attendance</Text>
-          </TouchableOpacity>
-
-          {/* Check Out Button */}
-          <TouchableOpacity 
-            style={[styles.button, styles.checkOutButton]} 
-            onPress={() => {}} // Implement check-out action later
-          >
-            <MaterialCommunityIcons name="exit-to-app" size={24} color="white" />
-            <Text style={styles.buttonText}>Check Out</Text>
-            <Text style={styles.buttonDescription}>Check Out for Attendance</Text>
+            <MaterialCommunityIcons name={isCheckedIn && !isCheckedOut ? "exit-to-app" : "clock"} size={24} color="white" />
+            <Text style={styles.buttonText}>{isCheckedIn && !isCheckedOut ? "Check Out" : "Check In"}</Text>
+            <Text style={styles.buttonDescription}>{isCheckedIn && !isCheckedOut ? "Check Out for Attendance" : "Check In for Attendance"}</Text>
           </TouchableOpacity>
 
           {/* Attendance History Button */}
           <TouchableOpacity 
             style={[styles.button, styles.historyButton]} 
-            onPress={navigateToattendance} // Implement attendance history action later
+            onPress={navigateToAttendance} // Implement attendance history action later
           >
             <MaterialCommunityIcons name="file-document" size={24} color="white" />
             <Text style={styles.buttonText}>Attendance History</Text>
             <Text style={styles.buttonDescription}>Click to view attendance log</Text>
           </TouchableOpacity>
 
-          {/* User List Button */}
-          <TouchableOpacity 
-            style={[styles.button, styles.historyButton]} 
-            onPress={navigateToUserList} // Implement user list action later
-          >
-            <MaterialCommunityIcons name="account-multiple" size={24} color="white" />
-            <Text style={styles.buttonText}>User List </Text>
-            <Text style={styles.buttonDescription}>Click to view all user</Text>
-          </TouchableOpacity>
+          {role !== 'employee' && (
+            <>
+              <TouchableOpacity 
+                style={[styles.button, styles.userListButton]} 
+                onPress={navigateToUserList} // Implement user list action later
+              >
+                <MaterialCommunityIcons name="account-multiple" size={24} color="white" />
+                <Text style={styles.buttonText}>User List</Text>
+                <Text style={styles.buttonDescription}>Click to view all users</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity 
+                style={[styles.button, styles.changeTimesButton]} 
+                onPress={navigateToUpdateTimes}
+              >
+                <MaterialCommunityIcons name="clock-edit" size={24} color="white" />
+                <Text style={styles.buttonText}>Change Times</Text>
+                <Text style={styles.buttonDescription}>Change check-in and check-out times</Text>
+              </TouchableOpacity>
+            </>
+          )}
         </View>
       </View>
-    );
+    </MenuProvider>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -104,40 +168,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#007AFF',
   },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-  },
-  card: {
+  menuIcon: {
+    padding: 10,
     backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 15,
+    borderRadius: 50,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  checkInCard: {
-    backgroundColor: '#d4f8d4',
-  },
-  checkOutCard: {
-    backgroundColor: '#f8d4d4',
-  },
-  historyCard: {
-    backgroundColor: '#d4e8f8',
-  },
-  cardTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
-    marginBottom: 5,
-  },
-  cardDescription: {
-    fontSize: 14,
-    color: '#555',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 2,
   },
   buttonContainer: {
     flexDirection: 'column', // Stack buttons vertically
@@ -161,6 +200,12 @@ const styles = StyleSheet.create({
   historyButton: {
     backgroundColor: '#2196F3', // Blue for history
   },
+  userListButton: {
+    backgroundColor: '#FF9800', // Orange for user list
+  },
+  changeTimesButton: {
+    backgroundColor: '#9C27B0', // Purple for change times
+  },
   buttonText: {
     color: 'white',
     fontSize: 18,
@@ -174,3 +219,23 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
 });
+
+const optionsStyles = {
+  optionsContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 2,
+  },
+  optionWrapper: {
+    padding: 10,
+  },
+  optionText: {
+    fontSize: 16,
+    color: '#333',
+  },
+};
